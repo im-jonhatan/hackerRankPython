694f547a48fbeb5be271cfe39e6979b6db32d319
Jonathan Prieto <jonathan@enlazar.net>
2020-08-19 21:18:24 -0500

    add some solutions for errors and exceptions

---
+++ errorsAndExceptions/exceptions.py
@@ -0,0 +1,35 @@
+# You are given two values a and b.
+# Perform integer division and print a/b.
+
+# Input Format
+# The first line contains T, the number of test cases.
+# The next T lines each contain the space separated values of a and b.
+
+# Constraints
+# • 0 < T < 10
+
+# Output Format
+# Print the value of a/b.
+# In the case of ZeroDivisionError or ValueError, print the error code.
+
+# Sample Input
+#     3
+#     1 0
+#     2 $
+#     3 1
+
+# Sample Output
+#     Error Code: integer division or modulo by zero
+#     Error Code: invalid literal for int() with base 10: '$'
+#     3
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        try:
+            a, b = map(int, input().split())
+            print(int(a/b))
+        except ZeroDivisionError:
+            print("Error Code: integer division or modulo by zero")
+        except BaseException as e:
+            print("Error Code: {}".format(e))
---
+++ errorsAndExceptions/incorrectRegex.py
@@ -0,0 +1,38 @@
+# You are given a string S.
+# Your task is to find out whether S is a valid regex or not.
+
+# Input Format
+# The first line contains integer T, the number of test cases.
+# The next T lines contains the string S.
+
+# Constraints
+# • 0 < T < 100
+
+# Output Format
+# Print "True" or "False" for each test case without quotes.
+
+# Sample Input
+#     2
+#     .*\+
+#     .*+
+
+# Sample Output
+#     True
+#     False
+
+# Explanation
+# .*\+ : Valid regex.
+# .*+: Has the error multiple repeat. Hence, it is invalid.
+
+
+import re
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        string = input()
+        try:
+            re.compile(string)
+            print(True)
+        except re.error:
+            print(False)
