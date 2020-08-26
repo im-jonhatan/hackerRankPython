c544ac623b17aaef65677612809c90974c6d10b3
Jonathan Prieto <jonathan@enlazar.net>
2020-08-26 10:13:56 -0500

    add solution for set mutations

---
+++ sets/setMutations.py
@@ -0,0 +1,53 @@
+# You are given a set A and N number of other sets. These N number of sets have to perform some specific mutation operations on set A.
+# Your task is to execute those operations and print the sum of elements from set A.
+
+# Input Format
+# The first line contains the number of elements in set A.
+# The second line contains the space separated list of elements in set A.
+# The third line contains integer N, the number of other sets.
+# The next 2*N lines are divided into N parts containing two lines each.
+# The first line of each part contains the space separated entries of the operation name and the length of the other set.
+# The second line of each part contains space separated list of elements in the other set.
+
+# Constrains
+# 0 < len(set(A)) < 1000
+# 0 < len(otherSets) < 100
+# 0 < N < 100
+
+# Output Format
+# Output the sum of elements in set .
+
+# Sample Input
+#     16
+#     1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
+#     4
+#     intersection_update 10
+#     2 3 5 6 8 9 1 4 7 11
+#     update 2
+#     55 66
+#     symmetric_difference_update 5
+#     22 7 35 62 58
+#     difference_update 7
+#     11 22 35 55 58 62 66
+
+# Sample Output
+#     38
+
+if __name__ == "__main__":
+    _ = int(input())
+    set1 = set(map(int, input().split()))
+    N = int(input())
+
+    for _ in range(N):
+        cmd, _ = input().split()
+        set2 = set(map(int, input().split()))
+        if(cmd == "intersection_update"):
+            set1.intersection_update(set2)
+        elif(cmd == "update"):
+            set1.update(set2)
+        elif(cmd == "symmetric_difference_update"):
+            set1.symmetric_difference_update(set2)
+        elif(cmd == "difference_update"):
+            set1.difference_update(set2)
+
+    print(sum(set1))
