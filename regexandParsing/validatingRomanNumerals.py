c5482dc394f5ae22502aaacfe24d20c354e16780
Jonathan Prieto <jonathanprietom@gmail.com>
2021-07-08 12:53:31 -0500

    add solution to Validating Roman Numerals problem

--- regexandParsing/validatingRomanNumerals.py
+++ regexandParsing/validatingRomanNumerals.py
@@ -16,6 +16,6 @@
 #     True
 
 import re
-regex_pattern = r"^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
 
 print(str(bool(re.match(regex_pattern, input()))))
